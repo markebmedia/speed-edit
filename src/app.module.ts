ef2a38a8462e736f67f2a56f2027b415f0f93c36
Mark Adiele <marklgs@Marks-MacBook-Pro.local>
2025-07-13 00:27:23 +0100

    Sanitize Stripe key and finalize payment integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,11 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-import { EnhanceController } from './enhance/enhance.controller';
-import { EnhanceService } from './enhance/enhance.service';
-
-import { StripeController } from './stripe/stripe.controller';
-import { StripeService } from './stripe/stripe.service';
+import { PaymentController } from './payment/payment.controller';
+import { PaymentService } from './payment/payment.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, EnhanceController, StripeController],
-  providers: [AppService, EnhanceService, StripeService],
+  controllers: [PaymentController],
+  providers: [PaymentService],
 })
 export class AppModule {}
-
